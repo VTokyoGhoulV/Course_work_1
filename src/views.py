f9d99d4e3d912729a12000f1eeceec90e946d9f9
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-05-28 21:53:46 +0300

    add get_currency_rates, python_to_json + small fixes

--- src/views.py
+++ src/views.py
@@ -1,23 +1,32 @@
-from datetime import datetime
-from collections import Counter
-import pandas as pd
 import json
+import os
+from collections import Counter
+from datetime import datetime
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
 
 from src.utils import xlsx_to_python
 
 transactions = xlsx_to_python("../data/operations.xlsx")
 
+with open("../user_settings.json", "r", encoding="utf-8") as file:
+    user_settings = json.load(file)
 
-def hello() -> str:
+user_stocks = user_settings["user_stocks"]
+
+
+def hello(time: datetime) -> str:
     """Возвращает приветствие в зависимости от текущего времени"""
 
-    if 0 <= datetime.now().hour < 6:
+    if 0 <= time.hour < 6:
         return "Доброй ночи!"
 
-    elif 6 <= datetime.now().hour < 12:
+    elif 6 <= time.hour < 12:
         return "Доброе утро!"
 
-    elif 12 <= datetime.now().hour < 18:
+    elif 12 <= time.hour < 18:
         return "Добрый день!"
 
     else:
@@ -28,9 +37,7 @@ def get_cards_info():
     """Возвращает данные по каждой карте"""
 
     unique_cards = {
-        transaction.get("Номер карты")
-        for transaction in transactions
-        if pd.notna(transaction.get("Номер карты"))
+        transaction.get("Номер карты") for transaction in transactions if pd.notna(transaction.get("Номер карты"))
     }
 
     for card in unique_cards:
@@ -39,10 +46,7 @@ def get_cards_info():
 
         for transaction in transactions:
 
-            if (
-                transaction.get("Номер карты") == card
-                and transaction.get("Сумма операции") < 0
-            ):
+            if transaction.get("Номер карты") == card and transaction.get("Сумма операции") < 0:
 
                 spend_counter += transaction.get("Сумма операции")
 
@@ -56,11 +60,10 @@ def get_cards_info():
 def get_top_transactions():
     """Возвращает топ 5 транзакций"""
 
-
     for transaction in sorted(
         transactions,
-        key=lambda transaction: transaction.get("Сумма операции"),
-        reverse=False,
+        key=lambda transaction_: abs(transaction_.get("Сумма операции", 0)),
+        reverse=True,
     )[:5]:
 
         yield {
@@ -68,15 +71,37 @@ def get_top_transactions():
             "amount": round(transaction.get("Сумма операции"), 2),
             "category": transaction.get("Категория"),
             "description": transaction.get("Описание"),
-
         }
 
 
-json_format = {
-    "greetings": hello(),
-    "cards": list(get_cards_info()),
-    "top_transactions": list(get_top_transactions()),
-}
+def get_currency_rates(currency: list):
+    """Возвращает актуальный курс валют по настройкам пользователя"""
 
-with open ("../user_settings.json", 'w', encoding="utf-8") as json_file:
-    json.dump(json_format, json_file, ensure_ascii=False, indent=2)
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols={",".join(currency)}&base=RUB"
+    headers = {"apikey": api_key}
+
+    request = requests.get(url, headers=headers)  # type: ignore
+
+    for currency in user_settings["user_currencies"]:
+        yield {"currency": currency, "rate": round(1 / request.json()["rates"][currency], 2)}
+
+
+def get_stocks_info():
+    """Возвращает актуальную цену акций по настройкам пользователя"""
+    pass
+
+
+def python_to_json():
+    """Переводит Python формат данных в JSON"""
+
+    json_format = {
+        "greetings": hello(datetime.now()),
+        "cards": list(get_cards_info()),
+        "top_transactions": list(get_top_transactions()),
+        "currency_rates": list(get_currency_rates(user_settings["user_currencies"])),
+    }
+
+    with open("../test.json", "w", encoding="utf-8") as json_file:
+        json.dump(json_format, json_file, ensure_ascii=False, indent=2)
