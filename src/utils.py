1159d6ad6b315570b836da5f5d4df039d38a5270
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-06-02 01:08:35 +0300

    add get_date_range

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from datetime import datetime, timedelta
 from pathlib import Path
+
+import pandas as pd
 
 
 def xlsx_to_python(file_path: str) -> list:
@@ -8,7 +10,7 @@ def xlsx_to_python(file_path: str) -> list:
     return data.to_dict("records")
 
 
-def find_project_root(marker_files=('pyproject.toml', '.git', 'requirements.txt')) -> Path:
+def find_project_root(marker_files=("pyproject.toml", ".git", "requirements.txt")) -> Path:
     """
     Ищет корневую директорию проекта, поднимаясь по дереву папок,
     пока не найдет один из маркерных файлов/папок.
@@ -19,3 +21,20 @@ def find_project_root(marker_files=('pyproject.toml', '.git', 'requirements.txt'
             if (parent / marker).exists():
                 return parent
     raise RuntimeError("Не удалось найти корень проекта. Убедитесь, что один из маркерных файлов присутствует.")
+
+
+def get_date_range(date_str: str, range_type="M"):
+    end_date = datetime.strptime(date_str, "%d.%m.%Y")
+
+    if range_type == "M":
+        start_date = end_date.replace(day=1)
+    elif range_type == "W":
+        start_date = end_date - timedelta(days=end_date.weekday())
+    elif range_type == "Y":
+        start_date = end_date.replace(month=1, day=1)
+    elif range_type == "ALL":
+        start_date = datetime(1900, 1, 1)  # пример
+    else:
+        raise ValueError("Неверный тип диапазона")
+
+    return start_date, end_date
