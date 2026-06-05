52a5ae2c2bd0ecdfbfad7969d34f04251d4a50e6
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-06-05 21:11:40 +0300

    add investment_bank

--- src/services.py
+++ src/services.py
@@ -1,16 +1,17 @@
 import json
+from datetime import datetime
 
-from src.utils import find_project_root, transactions
+from src.utils import find_project_root
 
 
-def get_the_best_cashback_categories(data: list, year: int, month: int) -> None:
+def get_the_best_cashback_categories(data: list, year: int, month_: int) -> None:
     """Возвращает список возможных кешбэков по категориям"""
 
     filtered_transactions = [
         transaction
         for transaction in data
         if transaction.get("Категория")
-        and f"{month}.{year}" in transaction.get("Дата операции")
+        and f"{month_}.{year}" in transaction.get("Дата операции")
         and transaction.get("Категория") not in ["Переводы", "Наличные", "Услуги банка"]
         and transaction.get("Сумма операции") < 0
         and transaction.get("Статус") != "FAILED"
@@ -31,5 +32,31 @@ def get_the_best_cashback_categories(data: list, year: int, month: int) -> None:
         json.dump(sorted_data, json_file, ensure_ascii=False, indent=2)
 
 
-if __name__ == "__main__":
-    get_the_best_cashback_categories(transactions, 2026, 5)
+def investment_bank(date: str, transaction_data: list[dict], limit: int) -> None:
+    """
+    Дату в формате YYYY.MM, информацию по транзакциям и лимит округления.
+    Возвращает суммы возможных округлений за месяц
+    """
+
+    target_date = datetime.strptime(date, "%Y.%m")
+
+    filtered_transactions = [
+        transaction
+        for transaction in transaction_data
+        if (operation_date := transaction.get("Дата операции"))
+        and (parsed_date := datetime.strptime(operation_date, "%d.%m.%Y %H:%M:%S"))
+        and parsed_date.year == target_date.year
+        and parsed_date.month == target_date.month
+        and transaction.get("Сумма операции") < 0
+        and transaction.get("Статус") != "FAILED"
+    ]
+
+    investment_counter = 0
+    for transaction in filtered_transactions:
+
+        if transaction.get("Сумма операции") % limit > 0:
+
+            investment_counter += transaction.get("Сумма операции") % limit
+
+    with open(f"{find_project_root()}/data/investment_bank.json", "w", encoding="utf-8") as file:
+        json.dump({"possible_investment": round(investment_counter, 2)}, file, ensure_ascii=False, indent=2)
